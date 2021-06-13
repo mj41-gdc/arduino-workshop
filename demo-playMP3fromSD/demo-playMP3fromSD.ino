ab1d1ac9c4633d0198fbd027ede8850dca4ee108
Michal Jurosz <michal.jurosz@gooddata.com>
2021-06-13 18:04:43 +0200

    Fixed "Multiple libraries" build issue
    Multiple libraries were found for "WiFiClient.h"
     Used: /home/mj/Arduino/libraries/WiFi101

--- demo-playMP3fromSD/demo-playMP3fromSD.ino
+++ demo-playMP3fromSD/demo-playMP3fromSD.ino
@@ -1,4 +1,10 @@
 #include <Arduino.h>
+
+#ifdef ESP32
+    #include <WiFi.h>
+#else
+    #include <ESP8266WiFi.h>
+#endif
 #include "AudioFileSourceSD.h"
 #include "AudioGeneratorMP3.h"
 #include "AudioOutputI2S.h"
